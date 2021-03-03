074209e214509698caacc5635e110199cccafd86
Jdelpinov <100429063@alumnos.uc3m.es>
2021-03-03 17:47:27 +0100

    Update mysize.c
    mysize first changes

--- mysize.c
+++ mysize.c
@@ -8,7 +8,34 @@
 
 int main(int argc, char *argv[])
 {
+	int infile;
+	DIR *directory;
+	struct dirent *dir;
+	char *currentdir[PATH_MAX];
+
+	getcwd(currentdir, PATH_MAX);
+	directory = opendir(currentdir);
 	
+	while((dir = readdir(directory)) != NULL){
+		if(dir.d_type == DT_REG){
+
+			infile = open(dir, O_RDONLY);
+
+			if (infile < 0){
+				return -1;
+			}
+			
+			int count;
+
+			count = lseek();
+			
+			close(infile);
+			printf("%s	%d", dir.d_name, count);
+		{
+
+	}
+
+	closedir(directory);
 
 	return 0;
 }
