01fcd338265c82acd469f5e336059f771d565b4e
ldcas-uc3m <100429021@alumnos.uc3m.es>
2021-03-03 16:24:22 +0100

    Luisda is trying

--- myls.c
+++ myls.c
@@ -4,11 +4,50 @@
 #include <dirent.h>
 #include <string.h>
 
+#define PATH_MAX 16  //TODO: set to a non-arbitrary number
 
-int main(int argc, char *argv[])
-{
-	
+int main(int argc, char *argv[]){
 
+	/*
+	---
+	SETTING DIRECTORY
+	---
+	*/
+
+	DIR *dir;
+	switch (argc){
+		case 0:
+			// no argument provided, use current directory
+			dir = getwd(dir);
+			if (dir == NULL){
+				printf("ERROR: Could not find current pathname.")
+			}
+			break;
+		case 1:
+			dir = argv[1];
+			break;
+		default:
+			// too many arguments
+			printf("ERROR: Too many arguments.\n");
+			return -1;
+	}
+
+	//TODO: fix this shit
+	int ddesc;
+	struct dirent *current;
+	while (ddesc = opendir(dir) != NULL){
+		current = readdir(dir);
+		printf("%s\n", current.d_name);
+	}
+	if (ddesc < 0){
+		printf("ERROR: Failed to open directory.\n");
+		return -1;
+	}
+
+	// struct dirent *pointer = readdir(dir);
+
+
+	closedir(ddesc);
 	return 0;
 }
 
