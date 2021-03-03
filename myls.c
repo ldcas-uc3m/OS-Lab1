19c84bca6b68779cb5fb4da55650db4b6fc90523
ldcas-uc3m <100429021@alumnos.uc3m.es>
2021-03-03 22:19:25 +0100

    Fix buffer
    Change from array of pointers to pointer of array; added header

--- myls.c
+++ myls.c
@@ -1,3 +1,8 @@
+/*
+@author: ldcas-uc3m
+@date: 03/03/2021
+*/
+
 #include <stdio.h>		/* Header file for system call printf */
 #include <unistd.h>		/* Header file for system call gtcwd */
 #include <sys/types.h>		/* Header file for system calls opendir, readdir y closedir */
@@ -12,12 +17,12 @@ int main(int argc, char *argv[]){ // TODO: Test & fix this shit
 	SETTING DIRECTORY
 	--- */
 
-	char *buff[PATH_MAX]; // Buffer to save the directory
+	char buff[PATH_MAX]; // Buffer to save the directory
 	DIR *dirp; // Main directory pointer
 	switch (argc){
 		case 0:
 			// no argument provided, use current directory
-			dirp = getcwd(buff, PATH_MAX); 
+			dirp = getcwd(&buff, PATH_MAX); 
 			if (dirp == NULL){
 				printf("ERROR: Could not find current pathname.");
 			}
@@ -45,7 +50,7 @@ int main(int argc, char *argv[]){ // TODO: Test & fix this shit
 	errno_t errno = 0; // error number "returned" from readdir(). If it's != 0, an error has ocurred
 	while (next = readdir(dirp) != NULL){
 		printf("%s\n", next->d_name); // using "->" instead of "." bc 'next' is a pointer
-		// TODO: add type
+		// TODO: add type?
 	}
 
 	if (errno != 0){
