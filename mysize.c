0b19a7ae0accf7bca6dfe27e8074cc76491fc905
Jdelpinov <100429063@alumnos.uc3m.es>
2021-03-03 21:32:12 +0100

    New changes

--- mysize.c
+++ mysize.c
@@ -11,16 +11,16 @@ int main(int argc, char *argv[])
 	int infile;
 	DIR *directory;
 	struct dirent *dir;
-	char *currentdir[PATH_MAX];
+	char currentdir[PATH_MAX];
 	
 	
 	getcwd(currentdir, PATH_MAX);
 	directory = opendir(currentdir);
 	
 	while((dir = readdir(directory)) != NULL){
-		if(dir.d_type == DT_REG){
+		if(dir->d_type == DT_REG){
 
-			infile = open(dir.d_name, O_RDONLY);
+			infile = open(dir->d_name, O_RDONLY);
 
 			if (infile < 0){
 				return -1;
@@ -31,13 +31,12 @@ int main(int argc, char *argv[])
 			position = lseek(infile, 0, SEEK_END);
 			
 			close(infile);
-			printf("%s	%ld", dir.d_name, position);
+			printf("%s	%ld\n", dir->d_name, position);
 		}
 
 	}
 
 	closedir(directory);
-
 
 	return 0;
 }
