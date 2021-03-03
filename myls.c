d207fb9631193f01ce5785e8e2b8442156f8b08f
ldcas-uc3m <100429021@alumnos.uc3m.es>
2021-03-03 18:55:55 +0100

    Pre-testing update
    Read the lab statement, kids. It's very usefull

--- myls.c
+++ myls.c
@@ -12,8 +12,8 @@ int main(int argc, char *argv[]){ // TODO: Test & fix this shit
 	SETTING DIRECTORY
 	--- */
 
-	char *buff[PATH_MAX]; //buffer to save the directory
-	DIR *dirp; // direction pointer
+	char *buff[PATH_MAX]; // Buffer to save the directory
+	DIR *dirp; // Main directory pointer
 	switch (argc){
 		case 0:
 			// no argument provided, use current directory
@@ -35,22 +35,21 @@ int main(int argc, char *argv[]){ // TODO: Test & fix this shit
 	MAIN LOOP
 	--- */
 
-	//TODO: test this shit
-	int ddesc = opendir(dirp);
+	DIR *ddesc = opendir(dirp); // Directory descriptor
 	if (ddesc == NULL){
 		printf("ERROR: Failed to open directory");
 		return -1;
 	}
 
 	struct dirent *next;
-	errno_t errno; // error number returned from readdir()
+	errno_t errno = 0; // error number "returned" from readdir(). If it's != 0, an error has ocurred
 	while (next = readdir(dirp) != NULL){
-		errno = 0;
 		printf("%s\n", next->d_name); // using "->" instead of "." bc 'next' is a pointer
+		// TODO: add type
 	}
 
 	if (errno != 0){
-		//NULL has been returned, because of an error
+		// NULL has been returned, because of an error
 		printf("ERROR: Failed to read from the directory. Error No. %i", errno);
 		return -1;
 	}
