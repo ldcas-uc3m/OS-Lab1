543c27939e000a3a0df0287bd4dfb8a42d4f00c1
ldcas-uc3m <100429021@alumnos.uc3m.es>
2021-03-04 09:40:25 +0100

    Working
    * PSA: when dealing wih arrays, the name of the variable is a pointer to the array.
    * errno_t is basically int, and gcc doesn't like errno_t, so I just swapped them and crossed my fingers.
    * some comments modification for aesthetics
    * Also, fucking parenthesis man.

--- myls.c
+++ myls.c
@@ -5,33 +5,30 @@
 
 #include <stdio.h>		/* Header file for system call printf */
 #include <unistd.h>		/* Header file for system call gtcwd */
-#include <sys/types.h>		/* Header file for system calls opendir, readdir y closedir */
+#include <sys/types.h>		/* Header file for system calls opendir, readdir & closedir */
 #include <dirent.h>
 #include <string.h>
 
-#define PATH_MAX 256  //TODO: set to a non-arbitrary number
-
-int main(int argc, char *argv[]){ // TODO: Test & fix this shit
+int main(int argc, char *argv[]){
 
 	/* ---
 	SETTING DIRECTORY
 	--- */
 
 	char buff[PATH_MAX]; // Buffer to save the directory
-	DIR *dirp; // Main directory pointer
 	switch (argc){
 		case 1:
-			// no argument provided, use current directory
-			dirp = getcwd(&buff, PATH_MAX); 
-			if (dirp == NULL){
+			/* no argument provided, use current directory */
+			getcwd(buff, PATH_MAX); // note to self: on buff[n], buff serves as the pointer
+			if (buff == NULL){
 				printf("ERROR: Could not find current pathname.");
 			}
 			break;
 		case 2:
-			dirp = strcpy(&buff, argv[1]);
+			strcpy(buff, argv[1]);
 			break;
 		default:
-			// too many arguments
+			/* too many arguments */
 			printf("ERROR: Too many arguments.\n");
 			return -1;
 	}
@@ -40,21 +37,20 @@ int main(int argc, char *argv[]){ // TODO: Test & fix this shit
 	MAIN LOOP
 	--- */
 
-	DIR *ddesc = opendir(dirp); // Directory descriptor
-	if (ddesc == NULL){
+	DIR *dirp = opendir(buff); // Directory pointer
+	if (dirp == NULL){
 		printf("ERROR: Failed to open directory");
 		return -1;
 	}
 
 	struct dirent *next;
-	errno_t errno = 0; // error number "returned" from readdir(). If it's != 0, an error has ocurred
-	while (next = readdir(dirp) != NULL){
+	int errno = 0; // error number "returned" from readdir(). If != 0, an error has ocurred
+	while ((next = readdir(dirp)) != NULL){
 		printf("%s\n", next->d_name); // using "->" instead of "." bc 'next' is a pointer
-		// TODO: add type?
 	}
 
 	if (errno != 0){
-		// NULL has been returned, because of an error
+		/* NULL has been returned, because of an error */
 		printf("ERROR: Failed to read from the directory. Error No. %i", errno);
 		return -1;
 	}
@@ -63,7 +59,7 @@ int main(int argc, char *argv[]){ // TODO: Test & fix this shit
 	CLOSE
 	--- */
 
-	closedir(ddesc);
+	closedir(dirp);
 	return 0;
 }
 
