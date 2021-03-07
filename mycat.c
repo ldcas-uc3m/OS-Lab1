45d687216d53051aab5d90de25397cc1a065a171
ldcas-uc3m <100429021@alumnos.uc3m.es>
2021-03-07 13:02:53 +0100

    Ignacio es estúpido

--- mycat.c
+++ mycat.c
@@ -21,27 +21,26 @@
 /* Global variables                         */
 /* ---------------------------------------- */
   
-char  buff_RX[MAX_BUFFER];           // Buffer to read the file 1024 Bytes
-char* fName = NULL;                    // Name of the File to read
-int   hFile;                           // Handle File 
+ 
 
 /* ---------------------------------------- */
 /* Main                                     */   
 /* ---------------------------------------- */
 int main(int argc, char* argv[])
 {
+	char  buff_RX[MAX_BUFFER];           // Buffer to read the file 1024 Bytes
+	char* fName = NULL;                    // Name of the File to read
+	int   hFile;                           // Handle File
 	int exitCode; // Code of the return of the program (RETURN_OK =0) or (RETURN_ERROR =-1)
 	int nBytes;   // Auxiliary variable to read the file
 	int flgRead;
 	
 	exitCode = RETURN_ERROR;
 	
-	if (argc < 2)
+	if (argc == 2)
 	  {
 	
 		fName = argv[1];
-
-		printf("Open file: %s...\n\n", fName);
 			
 		/* Open File */
 		hFile = open(fName, O_RDONLY);
@@ -52,13 +51,6 @@ int main(int argc, char* argv[])
 		    while ((nBytes = read(hFile, buff_RX, MAX_BUFFER )) > 0)  // A maximum of 1024 bytes from the file is copied ino the buffer in each turn
             {
 		     	mBytes = write(STDOUT_FILENO, buff_RX, MAX_BUFFER);
-				if(mBytes < 0)
-				{
-					printf("Error writting");
-					exitCode = RETURN_ERROR;
-				} else{
-					printf("%s", buff_RX);
-				}
 			}
 
 			if(nBytes == -1){
@@ -71,7 +63,10 @@ int main(int argc, char* argv[])
 			exitCode = RETURN_OK;
 		}
 		  
-  	  }
+  	}
+	else{
+		exitCode = RETURN_ERROR;
+	}
 
 	return(exitCode);
 }
